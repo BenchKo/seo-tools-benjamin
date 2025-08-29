3ba7e31ced7183df58b3dbbbe43600b6c95f2e86
benchko <benjaminkiewert@gmail.com>
2025-08-29 14:39:27 +0200

    ADD: Conversion for JetztBewerben Button

---
+++ Google-Tag/googleTag.js
@@ -0,0 +1,11 @@
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11396548672"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-11396548672');
+</script>
+
--- Google-Tag/googleTag_conversion_absenden.js
+++ Google-Tag/googleTag_conversion_absenden.js
@@ -1,36 +1,37 @@
-<!-- Event snippet for Kontakt (1) conversion page
-In your html page, add the snippet and call gtag_report_conversion when someone clicks on the chosen link or button. -->
-
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11396548672"> </script>
 <script>
-// First thinkz first Anonymisieren der Ips. Muss passieren bevor conversion reportiert wird!
-window.dataLayer = window.dataLayer || []; // window.datalayer nutzen oder window.dataLayer erstellen
 
-// arguments ist ein eingebautes Objekt Standard API von GoogleADS
-function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date()); // new Date()->Zeitstempel
-    //  Ip-Anonymisierung starten -> DSVGO_Konform -> Kein Banner nötig!
-    gtag('config', 'AW-11396548672', {
-        'anonymize_ip': true
-    }); // Objekte in {}?!
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+
+    gtag('js', new Date());
+    gtag('config', 'AW-11396548672', {anonymize_ip: true});
 
 
-// reportet Convs an GoogleAds
-function gtag_report_conversion() {
-    gtag('event', 'conversion', {
-      'send_to': 'AW-11396548672/hFg7CLDwioobEMCQproq',
+    // reportet Convs an GoogleAds
+    function gtag_report_conversion_contact() {
+        gtag('event', 'conversion', {
+        'send_to': 'AW-11396548672/hFg7CLDwioobEMCQproq',
+        'value': 1.0,
+        });
+    }
+
+    // Conversion wenn mail tatsächlich abgeschickt wird
+    document.addEventListener('wpcf7mailsent', function() {
+        console.log("Formular erfolgreich gesendet. \n");
+        gtag_report_conversion_contact();
     });
-    return false;
-}
-// Conversion wenn mail tatsächlich abgeschickt wird
-document.addEventListener('wpcf7mailsent',function(event) {
-    console.log("Formular erfolgreich gesendet. \n");
-    gtag_report_conversion()
-});
-// Wenn der Submit Knopf geklickt wird (ohne notwendigkeit von senden )
-document.querySelector('.wpcf7-submit').addEventListener('click', function(event) {
-  console.log("Button wurde geklickt!");
-});
-// 20/08/2025
+    function gtag_report_conversion_jetztBewerben() {
+        gtag('event', 'conversion', {
+            'send_to': 'AW-11396548672/1r8TCLm_kpAbEMCQproq',
+            'value': 1.0,
+        });
+    }
+    // conversion for the aplication button
+    const jetztBewerben = document.querySelector('a[href=mailto:bewerbung@between-the-lines.de"]');
+    jetztBewerben.addEventListener('click', function()){
+        gtag_report_conversion_jetztBewerben();
+    }
 </script>
 
 
